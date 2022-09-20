d594d8b3a7f57e79e3eae92aca735cebe083aa28
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-09-20 21:00:18 +0700

    update

--- C2109L/ADF1/20-09-2022/javade02/untitled/src/main/java/com/aptech/de02/ReadyMakeCake.java
+++ C2109L/ADF1/20-09-2022/javade02/untitled/src/main/java/com/aptech/de02/ReadyMakeCake.java
@@ -1,7 +1,21 @@
 package com.aptech.de02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ReadyMakeCake extends Cake{
     private Integer quantity;
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public ReadyMakeCake() {}
 
     public ReadyMakeCake(String name, Double rate, Integer quantity) {
         super(name, rate);
@@ -11,5 +25,16 @@ public class ReadyMakeCake extends Cake{
     @Override
     public double calculatePrice() {
         return rate * quantity;
+    }
+    @Override
+    public void input() {
+        super.input();
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter quantity: ");
+            this.quantity = Integer.valueOf(in.readLine());
+        }catch (IOException e) {
+            System.err.println("Input data error: "+e.getMessage());
+        }
     }
 }
