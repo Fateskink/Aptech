1a35f252eb784279cc6106ef52ab788c285c79e1
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-09-24 09:23:47 +0700

    update

---
+++ C2108G1/ADF1/24-09-2022/documentapp/src/main/java/aptech/data/manager/DocumentManager.java
@@ -0,0 +1,48 @@
+package aptech.data.manager;
+
+import aptech.data.impl.Book;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class DocumentManager {
+    private ArrayList<Book> books = new ArrayList<>();
+    void addDocument(){
+
+    }
+    void displayAllDocument(){
+
+    }
+    void searchByAuthorName(String authorName){
+        //common method
+        ArrayList<Book> booksResult = new ArrayList<>();
+        for (Book eachBook: this.books) {
+            if(eachBook.getAuthorName().toLowerCase().equals(authorName.toLowerCase())) {
+                booksResult.add(eachBook);
+            }
+        }
+        //use stream in Java8
+        booksResult = (ArrayList<Book>) this.books
+                .stream()
+                .filter(new Predicate<Book>() {
+                    @Override
+                    public boolean test(Book book) {
+                        return book.getAuthorName().toLowerCase().equals(authorName.toLowerCase());
+                    }
+                  })
+                .toList();
+        //use stream + lambda expression + oneline function
+        booksResult = (ArrayList<Book>) this.books
+                .stream()
+                .filter((Book book) -> book.getAuthorName()
+                        .toLowerCase()
+                        .equals(authorName.toLowerCase()))
+                .toList();
+        //show result
+        for (Book book: booksResult) {
+            book.show();
+        }
+    }
+
+}
