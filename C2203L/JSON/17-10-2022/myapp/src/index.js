adb423570669f5b6031c7b62ae0fa5ba26d790f4
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-10-17 19:50:30 +0700

    update

---
+++ C2203L/JSON/17-10-2022/myapp/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Welcome from './components/Welcome';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Welcome name={"Hoang"} x = {1} y = {2}/>
+  </React.StrictMode>
+);
+
+reportWebVitals();
