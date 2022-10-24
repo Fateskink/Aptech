dacf3f7262d8504e1fa7ccf174dd1337287b2c32
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-10-24 21:23:50 +0700

    update

--- C2203L/JSON/24-10-2022/myapp/src/index.js
+++ C2203L/JSON/24-10-2022/myapp/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Login } from './screens';
+import { Login, StudentManagement } from './screens';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <StudentManagement />
   </React.StrictMode>
 );
 
