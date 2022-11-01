0dc1230aaead4cf89387dc6c31da04ce99f7b0e0
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-11-01 21:21:07 +0700

    update

--- C2109L/JP2/01-11-2022/bookmanager/src/main/java/org/bookmanager/forms/BookManagerFrame.java
+++ C2109L/JP2/01-11-2022/bookmanager/src/main/java/org/bookmanager/forms/BookManagerFrame.java
@@ -1,5 +1,7 @@
 package org.bookmanager.forms;
 
+import org.bookmanager.database.Database;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -17,6 +19,8 @@ public class  BookManagerFrame extends JFrame{
     private JButton buttonAddNew;
     private JButton buttonShowAll;
     private JButton buttonDelete;
+
+    private Database database;
     public BookManagerFrame(String title) {
         this.setTitle(title);
         this.setContentPane(mainPanel);
@@ -29,13 +33,30 @@ public class  BookManagerFrame extends JFrame{
         this.setVisible(true);
         this.setupActions();
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        database = new Database();
+        try {
+            database.getConnection();
+        }catch (Exception e) {
+
+        }
     }
     private void setupActions() {
         buttonUpdate.addActionListener((ActionEvent actionEvent) -> {
             alert("Ban bam nut update");
         });
         buttonAddNew.addActionListener((ActionEvent actionEvent) -> {
-            alert("Ban bam nut buttonAddNew");
+            //alert("Ban bam nut buttonAddNew");
+            //get data from UI, validate ? Yes - homework
+            String code = textFieldBookCode.getText();
+            String name = textFieldBookName.getText();
+            //int categoryId = Integer.valueOf(comboBoxCategory.getName());
+            int categoryId = 1222;
+            float price = Float.valueOf(textFieldPrice.getText());
+            try {
+                database.insertBook(code, name, categoryId, price);
+            }catch (Exception e) {
+                alert(e.getMessage());
+            }
         });
         buttonShowAll.addActionListener((ActionEvent actionEvent) -> {
             //alert("Ban bam nut buttonShowAll");
