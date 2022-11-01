cb02553e64fb0ede2c64f88dee6b81ae84eed234
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-11-01 09:14:40 +0700

    update

---
+++ C2108G1/JP2/01-11-2022/albummanager/src/main/java/org/albummanager/Album.java
@@ -0,0 +1,42 @@
+package org.albummanager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+
+    private String name;
+    private List<Track> tracks = new ArrayList<Track>();
+    private List<Artist> musicians = new ArrayList<Artist>();
+    public Album() {
+
+    }
+
+    public Album(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    public List<Artist> getMusicians() {
+        return musicians;
+    }
+
+    public void setMusicians(List<Artist> musicians) {
+        this.musicians = musicians;
+    }
+}
---
+++ C2108G1/JP2/01-11-2022/albummanager/src/main/java/org/albummanager/TestAlbum.java
@@ -0,0 +1,37 @@
+package org.albummanager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestAlbum {
+    public static void main(String[] args) {
+        Artist tungDuong = new Artist("Tung Duong");
+        Artist quangTho = new Artist("Quang Tho");
+        Artist quangLe = new Artist("Quang Le");
+        Artist nhuQuynh = new Artist("Nhu Quynh");
+
+        //List<Track> track1 = new ArrayList<Track>();
+        //track1.add(new Track("Bai ca hi vong", 12000));
+        List<Track> track1 = Arrays.asList(
+                new Track("Bai ca hi vong", 12000),
+                new Track("Ao mua dong", 12000),
+                new Track("Ao mua dong", 130),
+                new Track("Que nha", 1500),
+                new Track("Me yeu con", 12300),
+                new Track("Me yeu con", 12300)
+        );
+        List<Track> track2 = Arrays.asList(
+                new Track("Ha buon", 500),
+                new Track("Vung la me bay", 1000),
+                new Track("Noi buon hoa phuong", 134),
+                new Track("Phuong hong", 10000)
+                );
+        Album album1 = new Album("Tung Duong hat tinh ca");
+        album1.setTracks(track1);
+        ArrayList<Artist> musicians = (ArrayList<Artist>)album1.getMusicians();
+        musicians.add(tungDuong);
+//        (album1.getMusicians()).add(new Artist());
+
+    }
+}
