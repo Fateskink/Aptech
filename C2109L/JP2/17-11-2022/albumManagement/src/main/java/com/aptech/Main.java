722dd600a70409c99dcf4f2314b6ec9f614c032d
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-11-17 19:50:31 +0700

    update

---
+++ C2109L/JP2/17-11-2022/albumManagement/src/main/java/com/aptech/Main.java
@@ -0,0 +1,10 @@
+package com.aptech;
+
+import com.aptech.models.Artist;
+
+public class Main {
+    public static void main(String[] args) {
+        Artist artist = new Artist("nbv");
+        System.out.println(artist);
+    }
+}
