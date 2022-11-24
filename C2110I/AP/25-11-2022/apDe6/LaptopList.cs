eccf22512d395dfe52f189819142f07aeea4c346
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-11-25 02:14:26 +0700

    update

---
+++ C2110I/AP/25-11-2022/apDe6/LaptopList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apDe6
+{
+    public class LaptopList
+    {
+        private List<Laptop> llist= new List<Laptop>();
+        private int capacity;
+        public LaptopList(int capacity) { 
+            this.capacity = capacity;
+            //indexer.Eg: objectA[2] 
+        }
+        public Laptop this[int i]
+        {
+            get  {
+                if (i < 0 || i >= capacity) {
+                    throw new IndexOutOfRangeException("overrange of capacity");
+                }
+
+                return llist[i];
+            }
+            set {
+                if (i < 0 || i >= capacity)
+                {
+                    throw new IndexOutOfRangeException("overrange of capacity");
+                }
+                llist[i] = value; 
+            }
+        }
+        int Count {
+            get => llist.Count;
+        }
+        public void AddLaptop(Laptop laptop) {
+            if (llist.Count >= this.capacity) {
+                throw new Exception("No capacity for the new laptop");
+            }
+            llist.Add(laptop);
+        }
+        public void ShowFilterLaptop(float maxPrice) {
+            llist.FindAll(laptop => laptop.PriceCal <= maxPrice)
+                .ForEach(laptop => {
+                    Console.WriteLine(laptop.ToString());
+                });
+        }
+    }
+}
---
+++ C2110I/AP/25-11-2022/apDe6/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using apDe6;
+
+public class Program {
+    public static void Main(string[] args) {
+        int capacity = 3;
+        LaptopList laptopList = new LaptopList(capacity);
+        laptopList.Input();
+    }
+}
+
