304050bbc25eefd2dcb9d52e69376db288760926
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-11-30 19:22:41 +0700

    ad

---
+++ C2203L/Java2/30-11-2022/ContactManagement/src/main/java/com/aptech/ContactForm.java
@@ -0,0 +1,17 @@
+package com.aptech;
+
+import javax.swing.*;
+
+public class ContactForm {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextArea textArea1;
+    private JComboBox comboBox1;
+    private JTable table1;
+    private JButton addButton;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JButton clearButton;
+}
