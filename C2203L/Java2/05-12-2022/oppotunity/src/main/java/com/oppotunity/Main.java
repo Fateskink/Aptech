84378c1e2455ea8aa3d635399e11390ad5307315
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-12-07 19:28:57 +0700

    update

--- C2203L/Java2/05-12-2022/oppotunity/src/main/java/com/oppotunity/Main.java
+++ C2203L/Java2/05-12-2022/oppotunity/src/main/java/com/oppotunity/Main.java
@@ -1,12 +1,13 @@
 package com.oppotunity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         Test test = new Test();
         test.inputOpportunities();
         test.saveToFile();
+        test.readFile();
+        test.showAllOpportunities();
+        test.findOpportunities();
+
     }
 }
