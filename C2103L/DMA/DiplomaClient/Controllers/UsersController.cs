03e87ee4f88a81ecb5e8ecf44363a880c6352a7d
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-02-14 21:07:09 +0700

    update

---
+++ C2103L/DMA/DiplomaClient/Controllers/UsersController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace DiplomaClient.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        private string SendPostRequest(string url, string postData)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "POST";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            //request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return responseFromServer;
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            string username = collection["username"];
+            string password = collection["password"];
+            //call api
+            string url = $"https://localhost:44315/api/Users/CheckLogin?UserName={username}&Password={password}";
+            //string postData = $"";
+            string jsonString = this.SendPostRequest(url, "");
+            Dictionary<string, string> dictResponse = JsonConvert
+                .DeserializeObject<Dictionary<string, string>>(jsonString);
+            if (!string.IsNullOrEmpty(dictResponse["UserName"]))
+            {
+                return RedirectToAction("ListOfUsers");
+            }
+            else {
+                //call api get all users
+                return RedirectToAction("Index");
+            }            
+            //return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult ListOfUsers() {
+            return View();
+        }
+        // POST: Users/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
