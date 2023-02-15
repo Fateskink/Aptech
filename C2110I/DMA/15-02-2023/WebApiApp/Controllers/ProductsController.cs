1dcae5058925e6448b23516283a91e00b1ab59a7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-02-15 16:29:57 +0700

    update

---
+++ C2110I/DMA/15-02-2023/WebApiApp/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiApp.Models;
+
+namespace WebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "iphone", "ipad" };
+        }
+        
+        [HttpGet("GetProducts")]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Description = "Description 1", Price = 10.99m },
+            new Product { Id = 2, Name = "Product 2", Description = "Description 2", Price = 19.99m },
+            new Product { Id = 3, Name = "Product 3", Description = "Description 3", Price = 25.99m }
+        };
+
+            return Ok(products);
+        }
+    }
+}
---
+++ C2110I/DMA/15-02-2023/WebApiApp/Models/MyDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApiApp.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
