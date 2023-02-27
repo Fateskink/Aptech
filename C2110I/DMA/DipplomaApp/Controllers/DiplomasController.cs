7e257318090766749ebcaa3421fe7acdcef1748f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-02-27 15:43:48 +0700

    update

---
+++ C2110I/DMA/DipplomaApp/Controllers/DiplomasController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DipplomaApp.Models;
+
+namespace DipplomaApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiplomasController : ControllerBase
+    {
+        private readonly DlmmsContext _context;
+
+        public DiplomasController(DlmmsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Diplomas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Diploma>>> GetDiplomas()
+        {
+          if (_context.Diplomas == null)
+          {
+              return NotFound();
+          }
+            return await _context.Diplomas.ToListAsync();
+        }
+
+        // GET: api/Diplomas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Diploma>> GetDiploma(int id)
+        {
+          if (_context.Diplomas == null)
+          {
+              return NotFound();
+          }
+            var diploma = await _context.Diplomas.FindAsync(id);
+
+            if (diploma == null)
+            {
+                return NotFound();
+            }
+
+            return diploma;
+        }
+
+        // PUT: api/Diplomas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDiploma(int id, Diploma diploma)
+        {
+            if (id != diploma.DiplomaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(diploma).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiplomaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Diplomas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Diploma>> PostDiploma(Diploma diploma)
+        {
+          if (_context.Diplomas == null)
+          {
+              return Problem("Entity set 'DlmmsContext.Diplomas'  is null.");
+          }
+            _context.Diplomas.Add(diploma);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDiploma", new { id = diploma.DiplomaId }, diploma);
+        }
+
+        // DELETE: api/Diplomas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiploma(int id)
+        {
+            if (_context.Diplomas == null)
+            {
+                return NotFound();
+            }
+            var diploma = await _context.Diplomas.FindAsync(id);
+            if (diploma == null)
+            {
+                return NotFound();
+            }
+
+            _context.Diplomas.Remove(diploma);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DiplomaExists(int id)
+        {
+            return (_context.Diplomas?.Any(e => e.DiplomaId == id)).GetValueOrDefault();
+        }
+    }
+}
