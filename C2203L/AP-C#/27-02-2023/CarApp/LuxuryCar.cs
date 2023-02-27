2af6493f0bf3881903ac2aad369931645a783560
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-02-27 19:21:31 +0700

    update

---
+++ C2203L/AP-C#/27-02-2023/CarApp/LuxuryCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    public class LuxuryCar:Car
+    {
+        private float specialRate = 0.8f;
+        public override float CalculatePrice() => base.CalculatePrice() + Price * specialRate;
+        public float CalculatePrice(float transportCost) =>
+            this.CalculatePrice() + transportCost;
+
+
+    }
+}
