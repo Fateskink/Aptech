a9562b79d6d0cb7d4f4ea845d5b6fef5e4350645
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-03-09 10:24:59 +0700

    update

---
+++ C2108G2/MVC/ECommerceApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceApp.ViewModels
+{
+    public class UserViewModel
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d).{6,}$", 
+            ErrorMessage = "Password must be at least 6 characters, " +
+            "at least 1 number, " +
+            "and first letter uppercase.")]
+        public string Password { get; set; }
+        
+    }
+}
