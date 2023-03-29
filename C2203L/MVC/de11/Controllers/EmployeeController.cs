767601d458e9f8543d116d0d5efca30dcf39502a
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-03-29 21:11:38 +0700

    update

--- C2203L/MVC/de11/Controllers/EmployeeController.cs
+++ C2203L/MVC/de11/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using de11.Models;
+using de11.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace de11.Controllers
@@ -12,7 +13,19 @@ namespace de11.Controllers
         public IActionResult Index()
         {
             var employees = context.Employees.ToList();
-            return View(employees);
+            ViewBag.Employees = employees;
+            ViewBag.Employee = new Employee { };
+            return View();
+        }
+        [HttpPost]
+        public IActionResult InsertEmployee(EmployeeViewModel employeeViewModel) {
+            this.context.Add(new Employee { 
+                EmployeeNo = employeeViewModel.EmployeeNo,
+                EmployeeName = employeeViewModel.EmployeeName,
+                Position = employeeViewModel.Position,
+                Salary = employeeViewModel.Salary
+            });
+            return RedirectToAction(nameof(Index));
         }
 
     }
