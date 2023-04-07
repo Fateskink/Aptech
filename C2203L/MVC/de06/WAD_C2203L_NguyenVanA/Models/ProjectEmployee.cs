3540e19e293d75f4dd36664010702314669130fa
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-04-07 21:01:59 +0700

    update

--- C2203L/MVC/de06/WAD_C2203L_NguyenVanA/Models/ProjectEmployee.cs
+++ C2203L/MVC/de06/WAD_C2203L_NguyenVanA/Models/ProjectEmployee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace WAD_C2203L_NguyenVanA.Models
 {
     public class ProjectEmployee
     {
+        [Key]
         public int EmployeeId { get; set; }
+        [Key]
         public int ProjectId { get; set; }
+        [Required]
         public string Tasks { get; set; }
         public virtual Employee Employee { get; set; }
         public virtual Project Project { get; set; }
