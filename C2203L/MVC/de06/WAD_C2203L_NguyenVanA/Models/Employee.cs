32c36f3a797dca715e7fac3c6b854d1761d84776
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-04-07 19:43:55 +0700

    update

---
+++ C2203L/MVC/de06/WAD_C2203L_NguyenVanA/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAD_C2203L_NguyenVanA.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime EmployeeDOB { get; set; }
+        public string EmployeeDepartment { get; set; }
+        public virtual ICollection<ProjectEmployee> ProjectEmployees { get; set; }
+    }
+}
---
+++ C2203L/MVC/de06/WAD_C2203L_NguyenVanA/Models/ProjectEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAD_C2203L_NguyenVanA.Models
+{
+    public class ProjectEmployee
+    {
+        public int EmployeeId { get; set; }
+        public int ProjectId { get; set; }
+        public string Tasks { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
