4962f0d82dbd251fa2463b8247f617f7b745b19e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-04-17 22:10:52 +0700

    update

--- C2108G2/DMA/myapp/StockApp.sql
+++ C2108G2/DMA/myapp/StockApp.sql
@@ -628,6 +628,14 @@ VALUES
 ('VN Financials Inverse ETF', 'VNFI3', 'VPBank', '2017-10-15');
 GO
 
+INSERT INTO top_stocks (stock_id, rank, reason) VALUES
+(1, 1, 'Doanh thu tăng trưởng mạnh'),
+(3, 2, 'Cổ tức cao'),
+(5, 3, 'Sản phẩm mới tiềm năng'),
+(7, 4, 'Điều chỉnh chiến lược kinh doanh'),
+(9, 5, 'Định giá hấp dẫn');
+GO
+
 INSERT INTO etf_holdings (etf_id, stock_id, shares_held, weight)
 VALUES
 (1, 1, 15000.1234, 0.065),
@@ -819,3 +827,66 @@ INSERT INTO transactions (user_id, linked_account_id, transaction_type, amount, 
 GO
 
 
+--create views
+
+CREATE VIEW vw_UserPortfolioSummary AS
+SELECT
+	p.user_id,
+	s.symbol,
+	s.company_name,
+	p.quantity,
+	p.purchase_price,
+	p.purchase_date
+FROM
+	portfolios p
+JOIN
+	stocks s ON p.stock_id = s.stock_id;
+GO
+
+-- Tổng quan danh mục đầu tư của người dùng
+-- Thêm giao dịch vào tài khoản ngân hàng liên kết
+CREATE PROCEDURE sp_AddTransaction
+(
+	@user_id INT,
+	@linked_account_id INT,
+	@transaction_type NVARCHAR(50),
+	@amount DECIMAL(18, 2)
+)
+AS
+BEGIN
+	INSERT INTO transactions (user_id, linked_account_id, transaction_type, amount, transaction_date)
+	VALUES (@user_id, @linked_account_id, @transaction_type, @amount, GETDATE());
+END;
+GO
+
+-- Tổng quan về các chỉ số thị trường
+CREATE VIEW vw_MarketIndicesSummary AS
+SELECT
+	mi.index_id,
+	mi.name,
+	mi.symbol,
+	COUNT(ic.stock_id) AS number_of_constituents
+FROM
+market_indices mi
+JOIN
+	index_constituents ic ON mi.index_id = ic.index_id
+GROUP BY
+mi.index_id, mi.name, mi.symbol;
+GO
+
+-- Tổng quan về các Quỹ Đầu Tư Chứng Khoán (ETF)
+CREATE VIEW vw_ETFsSummary AS
+SELECT
+	e.etf_id,
+	e.name,
+	e.symbol,
+	e.management_company,
+	e.inception_date,
+	COUNT(eh.stock_id) AS number_of_holdings
+FROM
+	etfs e
+JOIN
+	etf_holdings eh ON e.etf_id = eh.etf_id
+GROUP BY
+	e.etf_id, e.name, e.symbol, e.management_company, e.inception_date;
+GO
