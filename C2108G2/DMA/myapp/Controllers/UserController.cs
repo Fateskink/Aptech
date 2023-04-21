5c1ff3eccad4f018890bd7cb262ec5b93cef5c09
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-04-21 16:53:16 +0700

    update

--- C2108G2/DMA/myapp/Controllers/UserController.cs
+++ C2108G2/DMA/myapp/Controllers/UserController.cs
@@ -17,68 +17,39 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Cryptography;
+using myapp.Services;
 
 namespace myapp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
-    {
-        private readonly ApplicationDbContext _context;
-        private readonly IMapper _mapper;
-        private readonly IConfiguration _config;
+    {        
+        private readonly IUserService _userService;
 
-        public UserController(ApplicationDbContext context, IMapper mapper, IConfiguration config)
+        public UserController(IUserService userService)
         {
-            _context = context;
-            _mapper = mapper;
-            _config = config;
-        }       
+            _userService = userService;
+        }
         // POST: api/Users/Register
         [HttpPost("Register")]
         public async Task<ActionResult> Register([FromBody] RegisterViewModel registerViewModel)
         {
-            // Validate input and map UserViewModel to User
-            //var user = _mapper.Map<User>(registerViewModel);
+            User? newUser = await _userService.Register(registerViewModel);
 
-            // Call the stored procedure to register the user
-            var parameters = new[]
-                {
-                    new SqlParameter("@username", registerViewModel.Username),
-                    new SqlParameter("@password", registerViewModel.Password),
-                    new SqlParameter("@email", registerViewModel.Email),
-                    new SqlParameter("@phone", registerViewModel.Phone),
-                    new SqlParameter("@full_name", registerViewModel.FullName),
-                    new SqlParameter("@date_of_birth", registerViewModel.DateOfBirth),
-                    new SqlParameter("@country", registerViewModel.Country)
-                };
-
-            
-            var sql = "EXECUTE RegisterUser " +
-                            "@username, " +
-                            "@password, " +
-                            "@email, " +
-                            "@phone, " +
-                            "@full_name, " +
-                            "@date_of_birth, " +
-                            "@country";
-            IEnumerable<User> result = await _context.Users
-                    .FromSqlRaw(sql, parameters)
-                    .ToListAsync();
-            
-            User? newUser = result.FirstOrDefault(); //User? = "nullable User"            
             if (newUser != null)
             {
                 return CreatedAtAction("GetUser", new { id = newUser.UserId }, newUser);
             }
-            else {
+            else
+            {
                 return NotFound();
-            }                        
+            }
         }
-        //[HttpGet("{id}")]
+        [HttpGet("{id}")]        
         public async Task<ActionResult<User>> GetUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var user = await _userService.GetUserById(id);
 
             if (user == null)
             {
@@ -90,83 +61,18 @@ namespace myapp.Controllers
 
         // POST: api/Users/Login
         [HttpPost("Login")]
-        [AllowAnonymous]        
+        [AllowAnonymous]
         public async Task<IActionResult> Login(LoginViewModel loginViewModel)
         {
-            // Kiểm tra xem email và password có hợp lệ không
-            User? user = CheckLogin(loginViewModel.Email, loginViewModel.Password);
-            if (user == null)
+            try
             {
-                return BadRequest(new { message = "Email hoặc mật khẩu không chính xác" });
-            }            
-            // Tạo JWT token
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_config?.GetValue<string>("JwtSecret") ?? "");
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim("userId", user.UserId.ToString()),
-                    //new Claim("Email", user.UserId.ToString()),
-                }),
-                Expires = DateTime.UtcNow.AddDays(30),
-                SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(key),
-                    SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
-
-            // Kiểm tra số lượng device
-            var deviceCount = await _context.UserDevices.CountAsync(
-                    userDevice => userDevice.UserId == user.UserId);
-            
-            if (deviceCount >= 3)
-            {
-                // Cập nhật bản ghi đầu tiên
-                var firstDevice = await _context.UserDevices.FirstAsync(d => d.UserId == user.UserId);
-                firstDevice.DeviceId = loginViewModel.DeviceId;
-                firstDevice.Token = tokenString;
-                firstDevice.TokenExpiration = tokenDescriptor.Expires.Value;
-
-                await _context.SaveChangesAsync();
+                var token = await _userService.Authenticate(loginViewModel);
+                return Ok(new { token });
             }
-            else
+            catch (ApplicationException ex)
             {
-                if (_context.UserDevices
-                    .Where(userDevice => userDevice.DeviceId
-                        == loginViewModel.DeviceId).FirstOrDefault() != null)
-                {
-                    UserDevice existingUserDevice = _context.UserDevices
-                                                .Where(userDevice => userDevice.DeviceId
-                                                    == loginViewModel.DeviceId).FirstOrDefault()!;
-                    existingUserDevice.Token = tokenString;
-                    existingUserDevice.TokenExpiration = tokenDescriptor.Expires.Value;
-                    await _context.SaveChangesAsync();
-                }
-                else {
-                    // Thêm bản ghi mới
-                    var userDevice = new UserDevice
-                    {
-                        UserId = user.UserId,
-                        DeviceId = loginViewModel.DeviceId,
-                        Token = tokenString,
-                        TokenExpiration = tokenDescriptor.Expires.Value
-                    };
-                    _context.UserDevices.Add(userDevice);
-                    await _context.SaveChangesAsync();
-                }                
+                return BadRequest(new { message = ex.Message });
             }
-            // Trả về thông tin chi tiết của User kèm jwt token
-            return Ok(new { token = tokenString });
-        }
-        private User? CheckLogin(string email, string password)
-        {            
-            var sql = @"EXEC dbo.CheckLogin @Email, @Password;";
-            return _context.Users.FromSqlRaw(sql,
-                    new SqlParameter("@Email", email),
-                    new SqlParameter("@Password", password)
-                ).AsEnumerable()?.FirstOrDefault();
         }
 
     }
