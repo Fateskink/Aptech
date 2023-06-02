98afddb68eefa1c456114160cad480a316488a81
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-06-02 20:34:59 +0700

    update

---
+++ C2206L_C2204L/APC#/de07/CatManagement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace de07
+{
+    internal class CatManagement : IAction<Cat>
+    {
+        private List<Cat> catList = new List<Cat>();
+        public Cat this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void AddToList(Cat item)
+        {
+            catList.Add(item);
+        }
+
+        public void Display()
+        {
+            foreach(Cat item in catList)
+            {
+                item.DisplayData();
+            }
+        }
+    }
+
+}
