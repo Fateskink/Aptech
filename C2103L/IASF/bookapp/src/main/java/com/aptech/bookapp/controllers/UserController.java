945e25ec51d6c2a4265d87b3140457ae83ad90bc
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-07-27 20:18:02 +0700

    update

--- C2103L/IASF/bookapp/src/main/java/com/aptech/bookapp/controllers/UserController.java
+++ C2103L/IASF/bookapp/src/main/java/com/aptech/bookapp/controllers/UserController.java
@@ -3,12 +3,16 @@ package com.aptech.bookapp.controllers;
 import com.aptech.bookapp.models.User;
 import com.aptech.bookapp.services.IUserService;
 import com.aptech.bookapp.viewmodels.UserLoginViewModel;
+import com.aptech.bookapp.viewmodels.UserRegisterModel;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -19,19 +23,43 @@ public class UserController {
     private final IUserService userService;
     @GetMapping("/login")
     //view model = Data Transfer Object = Request Object
-    public String login(Model model) {
+    public ModelAndView login(Model model) {
         //model.addAttribute("book", 1book);//gủi dữ liệu từ controller => view
-        return "user/login"; //ten view: login.html
+        return new ModelAndView("user/login"); //ten view: login.html
+    }
+
+    @GetMapping("/register")
+    //view model = Data Transfer Object = Request Object
+    public ModelAndView register(Model model) {
+        model.addAttribute("userRegisterModel", new UserRegisterModel());
+        ModelAndView modelAndView = new ModelAndView("user/register"); // Đổi tên view thành tên view của bạn
+        modelAndView.addObject("userRegisterModel", new UserRegisterModel());
+        return modelAndView;
+    }
+    //Sau khi user bam nut "Register"
+    @PostMapping("/register")
+    public ModelAndView registerUser(@Valid UserRegisterModel model, BindingResult result) {
+        User newUser = userService.register(model);
+        if (newUser == null) {
+            ModelAndView modelAndView = new ModelAndView("user/register");
+            modelAndView.addObject("error", "Cannot register new user");
+            return modelAndView;
+        }
+        return new ModelAndView("user/login");
     }
     //sau khi bam login, gui reques post den ham nay
     @PostMapping("/login")
-    public String loginUser(UserLoginViewModel model) {
+    public ModelAndView loginUser(@Valid UserLoginViewModel model, BindingResult result) {
+
         //xử lý nghiệp vụ(gọi service)
         User user = userService.login(model.getUsername(), model.getPassword());
         //new sai mat khau hoac user ko ton tai
-        if(user == null) {
-            return "user/login"; //toi uu cach nao cho gui lai  @GetMapping("/login")
+        if (user == null) {
+            ModelAndView modelAndView = new ModelAndView("user/login");
+            modelAndView.addObject("error", "Invalid credentials or user does not exist.");
+            return modelAndView;
         }
-        return "home/index"; //ten view: login.html
+
+        return new ModelAndView("home/index"); // tên view: login.html
     }
 }
--- C2103L/IASF/bookapp/src/main/java/com/aptech/bookapp/services/UserService.java
+++ C2103L/IASF/bookapp/src/main/java/com/aptech/bookapp/services/UserService.java
@@ -2,6 +2,7 @@ package com.aptech.bookapp.services;
 
 import com.aptech.bookapp.models.User;
 import com.aptech.bookapp.repositories.UserRepository;
+import com.aptech.bookapp.viewmodels.UserRegisterModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,17 @@ public class UserService implements IUserService{
         //Khởi tạo Repo => DI (Dependency Injection)
         return userRepository.findByUsernameAndPassword(username, password)
                 .orElse(null);
+    }
+
+    @Override
+    public User register(UserRegisterModel model) {
+        //if(model.getPassword().equals(model.getRetypePassword())) => ko can
+        //phai dung "model validation"
+        User newUser = User.builder()
+                .password(model.getPassword())
+                .username(model.getUsername())
+                .dateOfBirth(model.getDateOfBirth())
+                .build();
+        return userRepository.save(newUser);
     }
 }
