9377c0740b47aa479cc2ab120af115cc7f7e2127
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-08-04 21:29:09 +0700

    update

---
+++ C2206L_C2204L/DMA/testapp/testapp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testapp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Double Price { get; set; }
+        public int Count { get; set; }
+    }
+}
