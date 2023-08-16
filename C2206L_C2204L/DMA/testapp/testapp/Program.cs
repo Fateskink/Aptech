b6246d83c8d876d69b6a22f5ec52a802d5d5173b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-08-16 20:46:19 +0700

    update

--- C2206L_C2204L/DMA/testapp/testapp/Program.cs
+++ C2206L_C2204L/DMA/testapp/testapp/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer2022")));
 builder.Services.AddScoped<DataSeeder>();
 
 builder.Services.AddCors(options =>
