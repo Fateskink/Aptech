bd9dcd4547371c515f823855effe0392165f0af9
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-08-18 09:03:00 +0700

    update

--- C2206L_C2204L/DMA/androidclient/app/src/main/java/com/aptech/androidclient/MainActivity.java
+++ C2206L_C2204L/DMA/androidclient/app/src/main/java/com/aptech/androidclient/MainActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.aptech.androidclient.adapters.ProductAdapter;
 import com.aptech.androidclient.models.Product;
+import com.aptech.androidclient.services.ProductApiService;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnReload;
@@ -34,6 +42,37 @@ public class MainActivity extends AppCompatActivity {
         });
         this.generateFakeProductData();
         listViewProducts = findViewById(R.id.listViewProducts);
+        /*
         productAdapter = new ProductAdapter(this,fakedProducts);
+        listViewProducts.setAdapter(productAdapter);
+        */
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://localhost:7020/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ProductApiService productApiService = retrofit.create(ProductApiService.class);
+
+        Call<List<Product>> call = productApiService.getProducts();
+
+        call.enqueue(new Callback<List<Product>>() {
+            @Override
+            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+                if (response.isSuccessful()) {
+                    List<Product> productList = response.body();
+                    //UI thread ?
+                    productAdapter = new ProductAdapter(MainActivity.this,fakedProducts);
+                    listViewProducts.setAdapter(productAdapter);
+                } else {
+                    // Handle error
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Product>> call, Throwable t) {
+                // Handle failure
+            }
+        });
     }
 }
--- C2206L_C2204L/DMA/androidclient/app/src/main/java/com/aptech/androidclient/adapters/ProductAdapter.java
+++ C2206L_C2204L/DMA/androidclient/app/src/main/java/com/aptech/androidclient/adapters/ProductAdapter.java
@@ -1,10 +1,16 @@
 package com.aptech.androidclient.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.aptech.androidclient.R;
 import com.aptech.androidclient.models.Product;
 
 import java.util.List;
@@ -19,4 +25,28 @@ public class ProductAdapter extends ArrayAdapter<Product> {
         this.products = products;
     }
 
+    @Override
+    public int getCount() {
+        return products.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem = convertView;
+        if (listItem == null) {
+            listItem = LayoutInflater.from(context).inflate(R.layout.product_item, parent, false);
+        }
+
+        Product currentProduct = products.get(position);
+
+        TextView textViewName = listItem.findViewById(R.id.textViewName);
+        textViewName.setText(currentProduct.getName());
+        TextView textViewPrice = listItem.findViewById(R.id.textViewPrice);
+        textViewPrice.setText(String.valueOf(currentProduct.getPrice()));
+
+        return listItem;
+        //return super.getView(position, convertView, parent);
+
+    }
 }
