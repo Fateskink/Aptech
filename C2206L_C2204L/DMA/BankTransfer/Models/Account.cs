4d6fae6efa9de9e4e364cd1af90c075280286f55
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-08-30 19:59:08 +0700

    update

---
+++ C2206L_C2204L/DMA/BankTransfer/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransfer.Models
+{
+    public class Account
+    {
+        [Key]
+        public int AccountID { get; set; }
+        public string AccountType { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "TotalAmount must be greater than 0")]
+        public double TotalAmount { get; set; }
+
+        // Foreign key to User
+        public int UserID { get; set; }
+        [ForeignKey("UserID")]
+        public User User { get; set; }
+
+    }
+}
---
+++ C2206L_C2204L/DMA/BankTransfer/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankTransfer.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPass { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+    }
+}
