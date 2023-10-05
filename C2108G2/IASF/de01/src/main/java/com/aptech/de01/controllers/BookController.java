d773c55f35494df423b72a088e664f503940d952
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-10-05 10:29:59 +0700

    update

--- C2108G2/IASF/de01/src/main/java/com/aptech/de01/controllers/BookController.java
+++ C2108G2/IASF/de01/src/main/java/com/aptech/de01/controllers/BookController.java
@@ -1,5 +1,8 @@
 package com.aptech.de01.controllers;
+import com.aptech.de01.models.Book;
+import com.aptech.de01.repositories.BookRepository;
 import com.aptech.de01.viewmodels.user.LoginViewModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,38 +10,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
+import java.util.List;
 
+
+@Controller
+@RequiredArgsConstructor
 public class BookController {
+    private final BookRepository bookRepository;
     @GetMapping("/books")
     public String home(Model model) {
         return "book/home";//view's name
     }
-    @PostMapping("/insert-book")
+    @PostMapping("/books/insert-book")
     public String insertBook(@ModelAttribute("book") Book book) {
         // Add logic to save the book to the database using your service
-        return "redirect:/books/list-all-books"; // Redirect to the list of books after insertion
+        return "book/list-all-books"; // Redirect to the list of books after insertion
+    }
+
+    @GetMapping("/books/insert-book")
+    public String createBook() {
+        // Add logic to save the book to the database using your service
+        return "book/insert-book"; // Redirect to the list of books after insertion
     }
 
     // Mapping to handle logout (assuming a POST request)
+
+    @GetMapping("/books/list-all-books")
+    public String listAllBooks(Model model) {
+        List<Book> books = bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "book/list-books";
+    }
+
     @PostMapping("/logout")
     public String logout() {
         // Add logic for logout, such as clearing the session or token
         return "redirect:/login"; // Redirect to the login page after logout
     }
 }
-/*
-* <ul>
-    <li>
-        <a href="@{/books/list-all-books}">List all book</a>
-    </li>
-    <li>
-        <a href="@{/books/insert-book}">Insert book</a>
-    </li>
-    <li>
-        <form method="post" action="@{/books/logout}">
-            <button type="submit">Logout</button>
-        </form>
-    </li>
-</ul>
-*/
--- C2108G2/IASF/de01/src/main/java/com/aptech/de01/controllers/UserController.java
+++ C2108G2/IASF/de01/src/main/java/com/aptech/de01/controllers/UserController.java
@@ -1,5 +1,8 @@
 package com.aptech.de01.controllers;
+import com.aptech.de01.models.User;
+import com.aptech.de01.repositories.UserRepository;
 import com.aptech.de01.viewmodels.user.LoginViewModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,8 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
     @GetMapping("/users/login")
     public String login(Model model) {
         model.addAttribute("loginViewModel", new LoginViewModel());
@@ -20,10 +27,16 @@ public class UserController {
             BindingResult bindingResult
     ) {
         if (bindingResult.hasErrors()) {
-            // Validation failed, return to the login form with error messages
             return "/users/login";
         }
-        return "redirect:/books";
+        //business
+        Optional<User> optionalUser = this.userRepository.findByUsernameAndPassword(
+                loginViewModel.getUsername(),
+                loginViewModel.getPassword());
+        if(optionalUser.isPresent()) {
+            return "redirect:/books";
+        }
+        return "user/login";//view's name
     }
 
 }
