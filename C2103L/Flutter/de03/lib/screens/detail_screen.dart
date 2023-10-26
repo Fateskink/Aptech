363ad843b449bb1adff30caa8a67d1b474b16913
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-10-26 19:58:38 +0700

    update

--- C2103L/Flutter/de03/lib/screens/detail_screen.dart
+++ C2103L/Flutter/de03/lib/screens/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:de03/models/product.dart';
 import 'package:de03/repositories/color_repository.dart';
+import 'package:de03/widgets/color_button.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
@@ -12,13 +13,26 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  List<String> colors = <String>[];
+  List<bool> isSelectedList = <bool>[];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final colorRepository = GetIt.instance<ColorRepository>();
+    setState(() {
+      colors = colorRepository.colors;
+      isSelectedList = List.filled(colors.length, false);
+      isSelectedList[0] = true;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    final colorRepository = GetIt.instance<ColorRepository>();
     return Scaffold(
       body: SafeArea(
         child: Container(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 height: 80,
@@ -52,6 +66,29 @@ class _DetailScreenState extends State<DetailScreen> {
                 this.widget.product.url,
                 width: MediaQuery.of(context).size.width - 20,
                 fit: BoxFit.cover,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20),
+                width: 200,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: colors.asMap().entries.map((entry) {
+                    final index = entry.key;
+                    final color = entry.value;
+
+                    return ColorButton(
+                      //color: Color(int.parse(color.replaceAll('#', ''), radix: 16)),
+                      color: Colors.red,
+                      isSelected: isSelectedList[index],
+                      onTap: (isSelected) {
+                        setState(() {
+                          isSelectedList = List.filled(colors.length, false);
+                          isSelectedList[index] = true;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
               )
             ],
           ),
