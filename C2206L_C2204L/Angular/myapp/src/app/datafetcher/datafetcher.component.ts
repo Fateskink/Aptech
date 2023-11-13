c936a6c33ea2212a70539b1570d55054ee97ba3f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-11-13 21:17:05 +0700

    add

--- C2206L_C2204L/Angular/myapp/src/app/datafetcher/datafetcher.component.ts
+++ C2206L_C2204L/Angular/myapp/src/app/datafetcher/datafetcher.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Product from '../../models/product';
+import {Product} from '../../models/product';
 import { 
   ProductService 
 } from '../services/product/product.service';
