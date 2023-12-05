7dd72929bfbf43dc1f451f9b4b34a7be19137a94
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-12-05 11:13:27 +0700

    update

---
+++ C2108G2/IDP/employee.dart
@@ -0,0 +1,12 @@
+class Employee {
+    String? id;
+    String name;
+    int workingDay;
+    double dailySalary;
+    Employee({
+        this.id,
+        required this.name,
+        required this.workingDay,
+        required this.dailySalary,
+    });
+}
