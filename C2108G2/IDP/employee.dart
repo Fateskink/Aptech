cc7c4de578fe94ec265b22aa2529815a75d1b639
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-12-07 11:43:10 +0700

    update

--- C2108G2/IDP/employee.dart
+++ C2108G2/IDP/employee.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Employee {
     String? id;
     String name;
@@ -9,4 +11,32 @@ class Employee {
         required this.workingDay,
         required this.dailySalary,
     });
+    factory Employee.input() {
+        print('Enter id: ');
+        String id = stdin.readLineSync() ?? "";
+
+        print('Enter name: ');
+        String name = stdin.readLineSync() ?? "";
+        int workingDay = 0;
+        while(true) {
+            print('Enter workingDay: ');
+            int workingDay = int.parse(stdin.readLineSync() ?? "0");
+            if(workingDay > 1 || workingDay < 31) {
+                break;
+            }
+        }
+
+        print('Enter dailySalary: ');
+        double dailySalary = double.parse(stdin.readLineSync() ?? "0");
+        return new Employee(id: id, name: name, workingDay: workingDay, dailySalary: dailySalary);
+    }
+    @override
+  String toString() {
+    // TODO: implement toString
+    return 'id: ${id}, '
+        'name: ${name}, '
+        'workingDay: ${workingDay}, '
+        'dailySalary: ${dailySalary}, '
+    ;
+  }
 }
--- C2108G2/IDP/main.dart
+++ C2108G2/IDP/main.dart
@@ -1,5 +1,57 @@
-int x = 10;
+import 'dart:collection';
+
+import 'employee.dart';
+import 'dart:io';
+
+List<Employee> employees = [];
 void main() {
-  print("chao ban");
-  print('Gia tri x ${x}');
+  int choice = 0;
+  while(choice != 5) {
+    print('-------------------------------------------------');
+    print('1.Input, 2.Sort, 3.Analyse, 4.Find, 5.Exit ');
+    print('Enter your choice: ');
+    print('-------------------------------------------------');
+    choice = int.parse(stdin.readLineSync() ?? "0");
+    switch(choice) {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      default:
+        if(choice != 5) {
+          print('Enter 1-4');
+        }
+    }
+  }
+}
+void input() {
+  print('Enter number of employees:');
+  int numberOfEmployees = int.parse(stdin.readLineSync() ?? "0");
+  employees.clear();
+  for(int i = 0; i < numberOfEmployees; i++) {
+    Employee employee = Employee.input();
+    employees.add(employee);
+  }
+}
+void sort() {
+  employees.sort((Employee e1, Employee e2) => e1.name.compareTo(e2.name));
+}
+void display() {
+  employees.forEach((employee) {
+    print(employee.toString());
+  });
+}
+void analyse() {
+  HashMap<int, int> hashMap = HashMap();
+  employees.forEach((employee) {
+    print(employee.toString());
+    hashMap[employee.workingDay] = hashMap.containsKey(employee.workingDay) ? hashMap[employee.workingDay]! + 1 : 1;
+  });
+  hashMap.forEach((key, value) {
+    print('$key: $value');
+  });
 }
