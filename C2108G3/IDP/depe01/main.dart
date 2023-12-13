fc53a58ac8ad86874e935a40c21c826c20cc1848
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-12-13 09:42:17 +0700

    update

--- C2108G3/IDP/depe01/main.dart
+++ C2108G3/IDP/depe01/main.dart
@@ -1,13 +1,10 @@
 
 import 'dart:io';
 import 'dart:math';
-import 'student.dart';
-import 'dart:convert';
 
 void main() {
   //bai02();
   //bai1c();
-  bai02();
 }
 void bai1b() {
   print('Input m = ');
@@ -43,21 +40,4 @@ void bai1c() {
 }
 void bai02(){
 
-  Student student = Student.input();
-  print(student.toString());
-  String jsonString = jsonEncode(student.toJson());
-  print('Chuỗi JSON: $jsonString');
-  
-  // d) Nhập xâu JSON và in ra chỉ số BMI và xếp hạng
-  // Yêu cầu người dùng nhập chuỗi JSON
-  print('\nNhập chuỗi JSON: ');
-  String jsonInput = stdin.readLineSync()!;
-  // Chuyển chuỗi JSON thành đối tượng Map
-  Map<String, dynamic> jsonMap = jsonDecode(jsonInput);
-  // Tạo đối tượng sinh viên từ dữ liệu Map
-  Student studentFromJson = Student.fromJson(jsonMap);
-  // Kiểm tra xem quá trình chuyển đổi từ JSON có thành công không
-  //  '{\"name\":\"n1\",\"age\":11,\"weight\":111.0,\"height\":11.0,\"gpa\":9.0}'
-  //{"name":"n1","age":11,"weight":111.0,"height":11.0,"gpa":9.0}
-  print(studentFromJson.toString());
 }
