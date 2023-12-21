276ef0d0a2b3918b79ff165c4561eea3f585c0ca
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-12-21 14:53:37 +0700

    update

---
+++ C2110iDoiCan/IASF/de02/src/main/java/com/aptech/de02/models/Product.java
@@ -0,0 +1,33 @@
+package com.aptech.de02.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Builder
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Product {
+    @Id
+    @Column(name = "product_id")
+    private String productId;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "description")
+    private String description;
+}
---
+++ C2110iDoiCan/IASF/de02/src/main/java/com/aptech/de02/services/category/ICategoryService.java
@@ -0,0 +1,9 @@
+package com.aptech.de02.services.category;
+
+import com.aptech.de02.models.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    List<Category> findAll();
+}
