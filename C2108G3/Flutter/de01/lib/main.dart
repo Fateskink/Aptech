502d07ac7f20165c6f08f1532502ffddd9d2784e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2023-12-25 09:44:05 +0700

    update

--- C2108G3/Flutter/de01/lib/main.dart
+++ C2108G3/Flutter/de01/lib/main.dart
@@ -1,20 +1,27 @@
-import 'package:de01/screens/product/insert.dart';
-import 'package:de01/screens/views/home/home.dart';
+import 'package:de01/repositories/product_repository.dart';
+import 'package:de01/screens/add_product_screen.dart';
+import 'package:de01/screens/main_screen.dart';
+import 'package:de01/screens/product_list.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 void main() {
+  GetIt.instance.registerLazySingleton<ProductRepository>(() => ProductRepository());
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      //home: HomeScreen(),
-      home: InsertProductScreen()
+      home: MainScreen()
+      //home: AddProductScreen()
+      //home: ProductListScreen()
     );
   }
 }
