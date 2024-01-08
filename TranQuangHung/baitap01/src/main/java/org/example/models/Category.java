341c1cc24125509ba5e8c7b7c0ee9161424590ea
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-01-08 16:25:37 +0700

    update

---
+++ TranQuangHung/baitap01/src/main/java/org/example/models/Category.java
@@ -0,0 +1,19 @@
+package org.example.models;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
+public class Category {
+    private Long id;
+    private String name;
+    private String description;
+}
+/*
+Bài tập:
+D
+* */
