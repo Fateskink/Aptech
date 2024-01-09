54a62fc42ac7604cc203813f2e783cadc3bf50a8
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-01-09 08:41:48 +0700

    update

--- TranQuangHung/baitap01/src/main/java/org/example/Main.java
+++ TranQuangHung/baitap01/src/main/java/org/example/Main.java
@@ -18,6 +18,8 @@ public class Main {
         System.out.println("haha");
         System.out.println(productB);
 
+
+
     }
     /*
     How to connect from Java Application to MySQL:
--- TranQuangHung/baitap01/src/main/java/org/example/models/Category.java
+++ TranQuangHung/baitap01/src/main/java/org/example/models/Category.java
@@ -15,5 +15,11 @@ public class Category {
 }
 /*
 Bài tập:
-D
+Download code baitap01 và làm thêm các chức năng:
+-Tao database trong mysql(tu cac cau lenh SQL hoc tren lop)
+-Hiền thị menu chức năng them, sua xoa, san pham
+VD: chon 1: them san pham, 2: edit san pham
+Du lieu nhap vao dung Scanner
+-Ket noi voi DB dung JDBC
+-Chuyen giao dien sang dinh dang JavaFX(chua can ngay)
 * */
