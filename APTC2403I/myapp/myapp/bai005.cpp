d9eb45d15b981a81b29aeda80278c8dfc2fff1b5
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-03-25 15:38:21 +0700

    update

--- APTC2403I/myapp/myapp/bai005.cpp
+++ APTC2403I/myapp/myapp/bai005.cpp
@@ -9,16 +9,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main5() {
     int sum = 0;
     for (int i = 9; i <= 300; i++) {
         //cout << "i = "<< i << endl;
-        if(i % 7 == 0 && i % 63 != 0) {
+        //if(i % 7 == 0 && i % 63 != 0) {
+        if(i % 2 == 0) {
             sum = sum + i;
         }
-        //tinh tong cac so chan
-        sum = sum + i;
         
     }
     cout << "sum = "<<sum<<"\n";
+    return 0;
 }
