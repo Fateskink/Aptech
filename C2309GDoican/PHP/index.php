3ae1bc51b4eb5bb60de9ef4abf98730ad098f435
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-03-26 11:08:14 +0700

    update

---
+++ C2309GDoican/PHP/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>chao ban</h1>
+<?php
+    echo "<h2 style='color:red;'>Hello, PHP!<h2>";
+?>
+</body>
+</html>
---
+++ C2309GDoican/PHP/sum.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Enter X and Y values:</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" 
+        method="get">
+        <label for="x">X:</label>
+        <input type="text" id="x" name="x"><br><br>
+        <label for="y">Y:</label>
+        <input type="text" id="y" name="y"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+    <?php
+        // Retrieve X and Y values from the POST request
+        $x = $_GET['x'] ?? 0;
+        $y = $_GET['y'] ?? 0;                
+        // Convert the values to integers
+        $x = intval($x);
+        $y = intval($y);
+
+        // Calculate the sum
+        $sum = $x + $y;
+
+        // Return the sum
+        echo "<h1>The sum of $x and $y is: $sum<h1>";
+        ?>
+</body>
+</html>
