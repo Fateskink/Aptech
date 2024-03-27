bd74f285af3e5be75f40a4bfa3f272cdcc2f745f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-03-27 14:32:26 +0700

    update

--- APTC2403I/myapp/myapp/bai008.cpp
+++ APTC2403I/myapp/myapp/bai008.cpp
@@ -20,7 +20,7 @@ int countDiv3(int arr[], int size) {
     return numberOfElements;
 }
 
-int main() {
+int main8() {
     int arr[] = {1, 3, 5, 6, 9, 12, 15, 90, 111, 22};
     int x = countDiv3(arr, 10);
     cout << "there are "<<x<<" elements divisible by 3"<<endl;
