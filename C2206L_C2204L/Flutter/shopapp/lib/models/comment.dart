f3d70f1bb3507c6935c5c1b0abc6cb8e99378ca6
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-04-12 20:05:42 +0700

    update

--- C2206L_C2204L/Flutter/shopapp/lib/models/comment.dart
+++ C2206L_C2204L/Flutter/shopapp/lib/models/comment.dart
@@ -1,23 +1,29 @@
+import 'package:foodapp/dtos/responses/user/user_response.dart';
+import 'package:foodapp/models/user.dart';
+
 class Comment {
   final int id;
   final int productId; // Corresponds to the product_id field in the Java class
-  final int userId; // Corresponds to the user_id field in the Java class
+  //final int userId; // Corresponds to the user_id field in the Java class
+  final UserResponse user;
   final String content;
   final DateTime createdAt;
 
   Comment({
     required this.id,
     required this.productId,
-    required this.userId,
+    required this.user,
     required this.content,
     required this.createdAt,
   });
 
   factory Comment.fromJson(Map<String, dynamic> json) {
+    UserResponse userResponse = UserResponse.fromJson(json['user']);
     return Comment(
       id: json['id'] as int,
       productId: json['product_id'] ?? 0, // Map to the product_id field in Java
-      userId: json['user_id'] ?? 0, // Map to the user_id field in Java
+      //userId: json['user_id'] ?? 0, // Map to the user_id field in Java
+      user: userResponse,
       content: json['content'] ?? '',
       createdAt: DateTime.parse(json['created_at']) ?? DateTime.now()
     );
--- C2206L_C2204L/Flutter/shopapp/lib/pages/detail_product/detail_product.dart
+++ C2206L_C2204L/Flutter/shopapp/lib/pages/detail_product/detail_product.dart
@@ -9,6 +9,7 @@ import 'package:foodapp/widgets/loading.dart';
 import 'package:foodapp/widgets/transparent_appbar.dart';
 import 'package:get_it/get_it.dart'; // Import the carousel_slider package
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 
 class DetailProduct extends StatefulWidget {
   final int productId;
@@ -34,100 +35,111 @@ class _DetailProductState extends State<DetailProduct> {
           context.go('/apptab');
         },
       ),
-      body: FutureBuilder<dynamic>(
-        future: productService.getProductById(widget?.productId ?? 0), // Call your API here
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Loading(); // Show loading indicator while waiting for data
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}')); // Show error message if there's an error`
-          } else {
-            // Data fetched successfully, display ListView
-            ProductResponse productResponse = snapshot.data!; // Extract products from the response
-            List<ProductImage> productImages = productResponse.productImages;
-            return SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CarouselSlider(
-                    items: productImages.map((productImage) {
-                      return Image.network(
-                        productImage.imageUrl,
-                        fit: BoxFit.cover,
-                      );
-                    }).toList(),
-                    options: CarouselOptions(
-                      aspectRatio: 16 / 9,
-                      enlargeCenterPage: true,
-                      autoPlay: true,
-                      autoPlayInterval: Duration(seconds: 3),
-                      autoPlayAnimationDuration: Duration(milliseconds: 800),
-                      autoPlayCurve: Curves.fastOutSlowIn,
-                    ),
+      body: SafeArea(
+        child: FutureBuilder<dynamic>(
+          future: productService.getProductById(widget?.productId ?? 0), // Call your API here
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Loading(); // Show loading indicator while waiting for data
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}')); // Show error message if there's an error`
+            } else {
+              // Data fetched successfully, display ListView
+              ProductResponse productResponse = snapshot.data!; // Extract products from the response
+              List<ProductImage> productImages = productResponse.productImages;
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CarouselSlider(
+                      items: productImages.map((productImage) {
+                        return Image.network(
+                          productImage.imageUrl,
+                          fit: BoxFit.cover,
+                        );
+                      }).toList(),
+                      options: CarouselOptions(
+                        aspectRatio: 16 / 9,
+                        enlargeCenterPage: true,
+                        autoPlay: true,
+                        autoPlayInterval: Duration(seconds: 3),
+                        autoPlayAnimationDuration: Duration(milliseconds: 800),
+                        autoPlayCurve: Curves.fastOutSlowIn,
+                      ),
 
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          productResponse.name,
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          'Price: \$'+'${productResponse.price}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: AppColors.primaryColor,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          productResponse.description,
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Icon(Icons.star, color: Colors.amber),
-                            Icon(Icons.star, color: Colors.amber),
-                            Icon(Icons.star, color: Colors.amber),
-                            Icon(Icons.star_half, color: Colors.amber),
-                            Icon(Icons.star_border, color: Colors.amber),
-                            SizedBox(width: 8),
-                            Text(
-                              '4.5 (1234 reviews)',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 16),
-                        Text(
-                          'User Comments:',
-                          style: Theme.of(context).textTheme?.bodyMedium?.copyWith(
-                              fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        ...productResponse.comments.map((comment) => ListTile(
-                          title: Text('User ${comment.content}',
-                              style: Theme.of(context).textTheme?.bodySmall?.copyWith(
-                              fontWeight: FontWeight.bold
-                          )),
-                          subtitle: Text(comment.createdAt.toString()),
-                        )),
-                      ],
                     ),
-                  ),
-                ],
-              ),
-            );
-          }
-        },
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            productResponse.name,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Price: \$'+'${productResponse.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            productResponse.description,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 16),
+                          Row(
+                            children: [
+                              Icon(Icons.star, color: Colors.amber),
+                              Icon(Icons.star, color: Colors.amber),
+                              Icon(Icons.star, color: Colors.amber),
+                              Icon(Icons.star_half, color: Colors.amber),
+                              Icon(Icons.star_border, color: Colors.amber),
+                              SizedBox(width: 8),
+                              Text(
+                                '4.5 (1234 reviews)',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            'User Comments:',
+                            style: Theme.of(context).textTheme?.bodyMedium?.copyWith(
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          ...productResponse.comments.map((comment) => Container(
+                            padding: EdgeInsets.only(top: 10),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('${comment.user.fullName}',
+                                      style: Theme.of(context).textTheme?.bodySmall?.copyWith(
+                                          fontWeight: FontWeight.bold
+                                      )),
+                                  Text('${comment.content}',
+                                      style: Theme.of(context).textTheme?.bodySmall),
+                                  Text(DateFormat('dd-MM-yyyy HH:mm:ss').format(comment.createdAt)),
+                                ]
+                            ),
+                          )),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+          },
+        ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
