7f196e25d3b9bd472cd7bbbd52404f54b36cd7c6
Nguyen Duc Hoang <hoangnd@Nguyens-Mac-mini.local>
2024-05-03 16:24:01 +0700

    update

--- C2206L_C2204L/Flutter/shopapp/lib/main.dart
+++ C2206L_C2204L/Flutter/shopapp/lib/main.dart
@@ -32,6 +32,7 @@ flutter pub add carousel_slider
 flutter pub add image_picker
 flutter pub add flutter_bloc
 flutter pub add flutter_facebook_auth
+Enable Google signin: https://console.firebase.google.com/project/shopapp-58f10/authentication/providers
 * */
 void main() {
   //register services
