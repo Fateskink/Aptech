a535c423378341fcd345d80357878e90e96f3c0d
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-05-08 11:40:29 +0700

    update

--- C2309GDoican/PHP/08-05-2024/de11/routes/web.php
+++ C2309GDoican/PHP/08-05-2024/de11/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalculationController;
+use Illuminate\Support\Facades\Route;;
 
 Route::get('/', function () {
     return view('welcome');//view ten la welcome
@@ -9,5 +9,6 @@ Route::get('/', function () {
 //Khi truy cập vào đường dẫn
 //http://localhost:8000/sum 
 //thì phương thức "index" của SumController sẽ thực hiện yêu cầu(request)
-Route::get('/sum', [CalculationController::class, 'index']);
+Route::get('/sum', [CalculationController::class, 'index'])->name('sum');
 //"class Route"::phương thức static, "get", called Facade
+Route::post('/calculate', [CalculationController::class, 'calculate'])->name('calculate');
